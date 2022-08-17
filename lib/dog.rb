b88e7e452ba253ba94730d92d89a905c013e057a
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-17 22:42:48 +0300

    Completd the lab

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+require "pry"
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit()
+        puts "The Dog is sitting"
+    end
+end
+
+
+funda = Dog.new
+fruda = Dog.new
+
+funda.bark
+fruda.bark
